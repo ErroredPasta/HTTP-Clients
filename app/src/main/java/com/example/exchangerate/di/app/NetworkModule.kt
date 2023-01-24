ee8d3f58f3e8d80c6e084101263ab6b4d5684df5
ErroredPasta <ForeignServices3132@pm.me>
2023-01-24 19:55:51 +0900

    NetworkModule.kt에 ExchangeRateApiImpl을 ExchangeRateApi로 bind하는 method 추가

--- app/src/main/java/com/example/exchangerate/di/app/NetworkModule.kt
+++ app/src/main/java/com/example/exchangerate/di/app/NetworkModule.kt
@@ -1,6 +1,9 @@
 package com.example.exchangerate.di.app
 
+import com.example.exchangerate.data.remote.ExchangeRateApi
+import com.example.exchangerate.data.remote.ExchangeRateApiImpl
 import com.google.gson.Gson
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
@@ -8,14 +11,20 @@ import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Singleton
 
 @Module
-object NetworkModule {
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }).build()
+interface NetworkModule {
 
-    @Provides
-    fun provideGson(): Gson = Gson()
+    @Binds
+    fun bindExchangeApi(impl: ExchangeRateApiImpl): ExchangeRateApi
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }).build()
+
+        @Provides
+        fun provideGson(): Gson = Gson()
+    }
 }
