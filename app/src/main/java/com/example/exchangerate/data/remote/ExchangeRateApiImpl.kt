0ac6df7e3daf1c1f198581e1553ed2c34e96c148
ErroredPasta <ForeignServices3132@pm.me>
2023-01-30 23:17:21 +0900

    Ktor client의 HttpClient를 이용하여 ExchangeRateApi 구현

--- app/src/main/java/com/example/exchangerate/data/remote/ExchangeRateApiImpl.kt
+++ app/src/main/java/com/example/exchangerate/data/remote/ExchangeRateApiImpl.kt
@@ -2,33 +2,19 @@ package com.example.exchangerate.data.remote
 
 import com.example.exchangerate.BuildConfig
 import com.example.exchangerate.data.dto.convertresult.ConversionResultDto
-import com.google.gson.Gson
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import io.ktor.client.*
+import io.ktor.client.request.*
 import javax.inject.Inject
 
 class ExchangeRateApiImpl @Inject constructor(
-    private val client: OkHttpClient,
-    private val gson: Gson
+    private val httpClient: HttpClient
 ) : ExchangeRateApi {
 
     override suspend fun convertCurrency(
         from: String,
         to: String,
         amount: String
-    ): ConversionResultDto {
-        val request = Request.Builder()
-            .get()
-            .url("${BASE_URL}/v6/${API_KEY}/pair/${from}/${to}/${amount}")
-            .build()
-
-        val response = client.newCall(request).execute()
-
-        return gson.fromJson(
-            response.body?.string(),
-            ConversionResultDto::class.java
-        )
-    }
+    ): ConversionResultDto = httpClient.get("${BASE_URL}/v6/${API_KEY}/pair/${from}/${to}/${amount}")
 
     companion object {
         /* TODO: 2022-12-15 목 02:25, Api key를 local.properties에 추가 */
