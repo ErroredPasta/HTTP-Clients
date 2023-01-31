e14d560b351541a5072718d20d26edc7ec2dda77
ErroredPasta <ForeignServices3132@pm.me>
2023-01-31 22:41:01 +0900

    runCoroutineCatching 제거

--- app/src/main/java/com/example/exchangerate/data/repository/ConversionRepositoryImpl.kt
+++ app/src/main/java/com/example/exchangerate/data/repository/ConversionRepositoryImpl.kt
@@ -2,14 +2,11 @@ package com.example.exchangerate.data.repository
 
 import com.example.exchangerate.data.mapper.toConversionResult
 import com.example.exchangerate.data.remote.ExchangeRateApi
-import com.example.exchangerate.domain.exception.ConversionException
 import com.example.exchangerate.domain.model.ConversionResult
 import com.example.exchangerate.domain.model.Currency
 import com.example.exchangerate.domain.repository.ConversionRepository
-import com.example.exchangerate.util.runCoroutineCatching
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
-import retrofit2.HttpException
 import javax.inject.Inject
 
 class ConversionRepositoryImpl @Inject constructor(
@@ -22,24 +19,10 @@ class ConversionRepositoryImpl @Inject constructor(
         to: Currency,
         amount: Double
     ): ConversionResult = withContext(ioDispatcher) {
-        return@withContext runCoroutineCatching {
-            api.convertCurrency(
-                from = from.toString(),
-                to = to.toString(),
-                amount = String.format("%f", amount)
-            ).toConversionResult()
-        }.getOrElse {
-            if (it is HttpException) {
-                whenHttpExceptionThrown(exception = it)
-            }
-
-            throw it
-        }
-    }
-
-    private fun whenHttpExceptionThrown(exception: HttpException) {
-        throw ConversionException.findExceptionInErrorBodyMessage(
-            message = exception.response()?.errorBody()?.string()!!
-        ) ?: exception
+        return@withContext api.convertCurrency(
+            from = from.toString(),
+            to = to.toString(),
+            amount = String.format("%f", amount)
+        ).toConversionResult()
     }
 }
