e283f12d05e5791b1be7e85eb1de6db9d048524b
ErroredPasta <ForeignServices3132@pm.me>
2023-02-06 23:29:05 +0900

    java.net의 HttpURLConnection을 이용하여 통신하도록 구현

--- app/src/main/java/com/example/exchangerate/data/remote/ExchangeRateApiImpl.kt
+++ app/src/main/java/com/example/exchangerate/data/remote/ExchangeRateApiImpl.kt
@@ -2,19 +2,42 @@ package com.example.exchangerate.data.remote
 
 import com.example.exchangerate.BuildConfig
 import com.example.exchangerate.data.dto.convertresult.ConversionResultDto
-import io.ktor.client.*
-import io.ktor.client.request.*
+import com.example.exchangerate.domain.exception.ConversionException
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import java.net.URL
 import javax.inject.Inject
+import javax.net.ssl.HttpsURLConnection
+
 
 class ExchangeRateApiImpl @Inject constructor(
-    private val httpClient: HttpClient
+    private val gson: Gson,
+    private val ioDispatcher: CoroutineDispatcher
 ) : ExchangeRateApi {
 
     override suspend fun convertCurrency(
         from: String,
         to: String,
         amount: String
-    ): ConversionResultDto = httpClient.get("${BASE_URL}/v6/${API_KEY}/pair/${from}/${to}/${amount}")
+    ): ConversionResultDto {
+        @Suppress("BlockingMethodInNonBlockingContext")
+        val getRequestConnection = withContext(ioDispatcher) {
+            URL("${BASE_URL}/v6/${API_KEY}/pair/${from}/${to}/${amount}").openConnection() as HttpsURLConnection
+        }.apply {
+            requestMethod = "GET"
+        }
+
+        val responseCode = getRequestConnection.responseCode
+        val responseBody = getRequestConnection.inputStream.bufferedReader().readText()
+
+        return if (responseCode == HttpsURLConnection.HTTP_OK) {
+            gson.fromJson(responseBody, ConversionResultDto::class.java)
+        } else {
+            throw ConversionException.findExceptionInErrorBodyMessage(responseBody)
+                ?: ConversionException.GeneralHttpException
+        }
+    }
 
     companion object {
         /* TODO: 2022-12-15 목 02:25, Api key를 local.properties에 추가 */
